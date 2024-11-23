627951d558aaa745b84255c7616e39c1b85b13ae
Tim Pastor <141689447+T-Pastor@users.noreply.github.com>
2024-11-22 21:01:51 -0500

    passed tests 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) { 
+    let finalString = '';
+    if(num < 0) {
+        return 0;
+    }
+    for (let i = 0; i < num; i++) {
+        finalString += word;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
