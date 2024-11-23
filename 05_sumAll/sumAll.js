e124fdfe8a8d931dfadec10edc13e8d53906d800
Tim Pastor <141689447+T-Pastor@users.noreply.github.com>
2024-11-23 18:41:35 -0500

    ininitial code to pass test 1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let numbers = [num1, num2];
+    let sum = 0;
 
-};
+    for (let i = 0; i < numbers.length; i++) {
+        if (!typeof numbers.at(i) === 'number' || !numbers.at(i) > 0) {
+            return 'ERROR'
+        }
+    }
+
+    numbers.sort(function(a, b) {return a - b});
+
+
+
+    for (let i = numbers.at(0); i <= numbers.at(1); i++) {
+        sum += i;
+    }
+
+    return sum;
+}
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
