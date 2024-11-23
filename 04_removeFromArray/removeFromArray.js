6175375c18c0871ae8801d72c468bc2bf121c148
Tim Pastor <141689447+T-Pastor@users.noreply.github.com>
2024-11-23 17:33:49 -0500

    modified code to pass test 3

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,12 @@
 const removeFromArray = function(array, ...value) {
 
    for (let i = 0; i < value.length; i++) {
-        for(let j = 0; j < array.length; j++) {
+        for(let j = 0; j < array.length;) {
             if (value.at(i) === array.at(j)) {
                 array.splice(j, 1);
+                j = j;
+            } else {
+                j++;
             }
         }
     }
