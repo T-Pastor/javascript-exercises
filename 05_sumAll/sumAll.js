7e6721fab6c721eff05629d4f3f5691c88d769ee
Tim Pastor <141689447+T-Pastor@users.noreply.github.com>
2024-11-23 18:53:57 -0500

    modified validation error to pass test 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,7 +3,7 @@ const sumAll = function(num1, num2) {
     let sum = 0;
 
     for (let i = 0; i < numbers.length; i++) {
-        if (!typeof numbers.at(i) === 'number' || numbers.at(i) < 0 || numbers.at(i) % 1 > 0) {
+        if (typeof numbers.at(i) !== 'number' || numbers.at(i) < 0 || numbers.at(i) % 1 > 0) {
             return 'ERROR';
         }
     }
