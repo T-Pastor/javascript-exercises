0cfa03c7df3fe15e26230d5e043be82daa41d45a
Tim Pastor <141689447+T-Pastor@users.noreply.github.com>
2024-11-23 15:16:04 -0500

    code to pass only test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, value) {
+    let index = undefined;
+    
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === value) {
+            index = i;
+            break;
+        }
+    }
+
+    return array.toSpliced(index, 1);
 };
 
 // Do not edit below this line
