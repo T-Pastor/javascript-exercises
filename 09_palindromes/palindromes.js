004946b72da9e4641512ff812eda3c747afcf23c
Tim Pastor <141689447+T-Pastor@users.noreply.github.com>
2024-11-23 20:49:40 -0500

    initial code for testing

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (expression) {
+    let reverseString = "";
+    let tempString = "";
+    let formattedString = "";
 
+    tempString = expression.toLowerCase();
+
+    formattedString = tempString.replace(/[^A-Za-z]/g, '');
+
+    for (let i = formattedString.length - 1; i >=0; i--) {
+        reverseString += formattedString[i];
+    }
+
+    if (reverseString === formattedString) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
