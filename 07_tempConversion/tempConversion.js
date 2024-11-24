25a7a70443cc87b870792f78989cc55f78ef7864
Tim Pastor <141689447+T-Pastor@users.noreply.github.com>
2024-11-23 19:31:39 -0500

    initial code

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesF) {
+  let celsius = (degreesF - 32) / (9 / 5);
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesC) {
+  let farenheit = (degreesC * (9 / 5)) + 32;
+  return parseFloat(farenheit.toFixed(1));
 };
 
 // Do not edit below this line
