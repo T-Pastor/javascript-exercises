94fb3c5ff9585bff4dc5a8088eb7380eeb15a0d4
Tim Pastor <141689447+T-Pastor@users.noreply.github.com>
2024-11-23 21:27:17 -0500

    initial code for testing

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fib = [1, 1];
 
+    for (let i = 2; i < num; i++) {
+            
+        let nextNumber = fib.at(i - 1) + fib.at(i - 2);
+        fib.push(nextNumber);
+    }
+
+    return fib.at(fib.length - 1);
 };
 
 // Do not edit below this line
