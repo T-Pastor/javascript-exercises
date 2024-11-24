0c5ce76c95ba096a58de46c3caab40a83b402be8
Tim Pastor <141689447+T-Pastor@users.noreply.github.com>
2024-11-24 16:18:19 -0500

    code for attempting to pass the test, would not pass due to returning an extra array. could not find solution.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,17 @@
-const getTheTitles = function() {
+const getTheTitles = function(obj) {
+    //convert key/value pairs to array
+    let entries = Object.entries(obj);
+    let titles = [];
 
+    //iterate through key/value pairs and get the value of only the titles
+    entries.filter(() => {
+        if(entries.keys === 'title') {
+            titles.push(entries.title);
+        }
+    })
+
+   // return the titles
+    return titles;
 };
 
 // Do not edit below this line
