085124f3583c1eee190c8da7dc885057a2b1c71e
Tim Pastor <141689447+T-Pastor@users.noreply.github.com>
2024-11-24 19:47:28 -0500

    code modified for test 3 with current year validation

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -2,13 +2,23 @@ const findTheOldest = function(people) {
     let oldestAge = 0;
     let oldestPerson = "";
     let index = 0;
+    let currentYear = new Date().getFullYear();
 
     for (let person in people) {
-        let age = people[person].yearOfDeath - people[person].yearOfBirth;
-        if (age > oldestAge) {
-            oldestAge = age;
-            oldestPerson = people[person].name;
-            index = person;
+        if (!Object.keys(people[person]).includes('yearOfDeath')) {
+            let age = currentYear - people[person].yearOfBirth;
+            if (age > oldestAge) {
+                oldestAge = age;
+                oldestPerson = people[person].name;
+                index = person;
+            }
+        } else {
+            let age = people[person].yearOfDeath - people[person].yearOfBirth;
+            if (age > oldestAge) {
+                oldestAge = age;
+                oldestPerson = people[person].name;
+                index = person;
+            }
         }
     }
     return people[index];
