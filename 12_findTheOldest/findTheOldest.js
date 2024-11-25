35a1b47f2a533b4fc904c181c4ed5884fa147608
Tim Pastor <141689447+T-Pastor@users.noreply.github.com>
2024-11-24 19:14:50 -0500

    initial code to pass the first test. it isnt pretty but it works. need to understand the reduce method

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestAge = 0;
+    let oldestPerson = "";
+    let index = 0;
 
-};
-
+    for (let person in people) {
+        let age = people[person].yearOfDeath - people[person].yearOfBirth;
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPerson = people[person].name;
+            index = person;
+        }
+    }
+    return people[index];
+}
 // Do not edit below this line
 module.exports = findTheOldest;
