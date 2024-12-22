d69623d94863aa7e04b0bfbd279d80020b9bae58
Tim Pastor <141689447+T-Pastor@users.noreply.github.com>
2024-12-22 16:14:36 -0500

    updated with array method

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,17 +1,5 @@
 const getTheTitles = function(obj) {
-    //convert key/value pairs to array
-    let entries = Object.entries(obj);
-    let titles = [];
-
-    //iterate through key/value pairs and get the value of only the titles
-    entries.filter(() => {
-        if(entries.keys === 'title') {
-            titles.push(entries.title);
-        }
-    })
-
-   // return the titles
-    return titles;
+    return obj.map(obj => obj.title);
 };
 
 // Do not edit below this line
